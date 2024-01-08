6499f598ea9e8b4b697a7c874374f50b480f46b6
enricoforesta <enrico.foresta97@gmail.com>
2024-01-08 16:00:26 +0100

    feat: index e result

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP Badwords</title>
 </head>
+
 <body>
-    <h1>index</h1>
+   <div class="container">
+      <!-- form -->
+      <form action="result.php" method="get">
+         <label for="text"> inserisci il paragrafo:</label>
+         <textarea name="text" id="text" cols="30" rows="10"></textarea>
+         <label for="censured">Inserisci la parola da censurare:</label>
+         <input type="text" id="censured" name="censured">
+         <input type="submit" value="Invia">
+      </form>
+      <!-- / form -->
+   </div>
 </body>
+
 </html>
--- result.php
+++ result.php
@@ -1,11 +1,54 @@
+<!-- Variabili  -->
+<?php
+$text = $_GET["text"];
+$censured = $_GET["censured"];
+$text_censured = str_replace($censured, "***", $text);
+?>
+<!-- / Variabili  -->
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Result</title>
 </head>
+
 <body>
-    <h1>risultato</h1>
+    <div class="result">
+        <!-- paragrafo iniziale -->
+        <div>
+            <h2>paragrafo iniziale: </h2>
+            <p>
+                <?php
+                echo $text;
+                ?>
+            </p>
+            <span>Il paragrafo è lungo
+                <?php
+                echo strlen($text);
+                ?>
+            </span>
+        </div>
+        <!-- / paragrafo iniziale -->
+        <!-- paragrafo censurato -->
+        <div>
+            <h2>paragrafo censurato: </h2>
+            <p>
+                <?php
+                echo $text_censured;
+                ?>
+            </p>
+            <span>Il paragrafo è lungo
+                <?php
+                echo strlen($text_censured);
+                ?>
+            </span>
+        </div>
+        <!-- / paragrafo censurato -->
+    </div>
 </body>
+
 </html>
